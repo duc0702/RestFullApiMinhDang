2b5792b806d473559eb1854e563da6aab7010868
Nguyen Duc <dunguyen07022004@gmail.com>
2025-03-03 15:28:54 +0700

    Update

---
+++ BuoiHoc/buoi01/src/main/java/com/example/buoi01/config/SercurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.example.buoi01.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SercurityConfiguration {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    private String[] routeList = { "/", "/auth/**" };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(c -> c.disable())
+                .authorizeHttpRequests(authz ->
+                        // prettier-ignore
+
+                        authz
+                                .requestMatchers(routeList).permitAll()
+                                .anyRequest().authenticated()
+
+
+//                //! Khai báo JWT
+//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+//                .formLogin(f -> f.disable())
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        );
+        return http.build();
+    }
+}
